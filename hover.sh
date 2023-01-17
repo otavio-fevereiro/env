cf5f91d42a1f50357c60e1e97574bd53baf7e225
otavio-fevereiro <otavio.fevereiro@nubank.com.br>
2023-01-17 08:23:13 -0300

    update

--- hover.sh
+++ hover.sh
@@ -1,6 +1,6 @@
 cd ~/dev/nu/mini-meta-repo
 flutter pub get
-monocli deps get -p flutter
+#monocli deps get -p flutter
 monocli deps get -p flutter-desktop
 monocli l10n get
 export HOVER_HEIGHT=480
