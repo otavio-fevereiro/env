97a9fc739cf2ac6009f9817e4950ec651fe666c1
otavio-fevereiro <otavio.fevereiro@nubank.com.br>
2024-04-19 08:40:56 -0300

    updates

--- random.sh
+++ random.sh
@@ -7,3 +7,5 @@ sudo kill -9 $(sudo lsof -t -i:5095)
 sudo lsof -i -P | grep LISTEN | grep :$PORT
 
 59515
+
+nu ser curl DELETE S2 shore /ops/graphql-cache/key/59f08dd1-2a29-4cc8-8f53-72b6bac7f59f/get-app-state/shell-mobile
--- update-credentials.sh
+++ update-credentials.sh
@@ -2,7 +2,10 @@
 #nu aws credentials refresh   --maven-login    
 
 nu update
-nu aws credentials refresh  
+#nu aws credentials setup
+nu aws credentials refresh
 nu codeartifact login maven
 #nu cert setup --env staging
 #nu cert setup --env prod
+
+#nu-co-staging sec scope add otavio.fevereiro admin experiment-management
