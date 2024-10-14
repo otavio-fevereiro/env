5d51c95ba2b3796503388b3309b34f32af037ee7
otavio-fevereiro <otavio.fevereiro@nubank.com.br>
2024-10-14 09:28:38 -0300

    a

--- update-credentials.sh
+++ update-credentials.sh
@@ -1,12 +1,11 @@
-#nu aws credentials setup
-#nu aws credentials refresh   --maven-login    
-
 nu update
 #nu aws credentials setup
+#nu-br aws credentials refresh --npm-login
 nu aws credentials refresh
-#nu aws shared-role-credentials refresh --account-alias=br --keep-policies eng,prod-eng
+nu aws shared-role-credentials refresh --account-alias=br --keep-policies eng,prod-eng,casual-dev,homepage-sandbox,first-layer-infra-and-monitoring-admin
+nu aws shared-role-credentials refresh --account-alias=br-staging --keep-policies eng,prod-eng,casual-dev,homepage-sandbox,first-layer-infra-and-monitoring-admin
+#nu aws shared-role-credentials web-console --account-alias=br --keep-policies eng,casual-dev,prod-eng
 nu codeartifact login maven
 #nu cert setup --env staging
 #nu cert setup --env prod
 
-#nu-co-staging sec scope add otavio.fevereiro admin experiment-management
